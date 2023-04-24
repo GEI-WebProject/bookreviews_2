76bba78f61e8393a34196df3fa903edbc5f1e7b6
Mihaela <mbuturuga@gmail.com>
2023-04-24 12:40:29 +0200

    added __str__ method to models

--- bookapp/models.py
+++ bookapp/models.py
@@ -4,9 +4,16 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+         return self.name
     
 class Language(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+         return self.name
+    
     
 class Book(models.Model):
     name = models.CharField(max_length=50)
@@ -14,13 +21,22 @@ class Book(models.Model):
     sinopsys = models.CharField(max_length=200)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+         return self.name
     
 class Author(models.Model):
     name = models.CharField(max_length=50)
     birth_date = models.DateField()
+
+    def __str__(self):
+         return self.name
     
 class Publisher(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+         return self.name
     
 class Publishes(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
