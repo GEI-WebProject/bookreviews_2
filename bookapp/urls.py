25cd3c307ba4b5976beffc9f54bdecdaf6b2a220
Mihaela <mbuturuga@gmail.com>
2023-04-24 23:16:55 +0200

    created BookDetailView and html

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('book/<int:pk>', BookDetailView.as_view(), name='book_detail'),
 ]
--- bookapp/views.py
+++ bookapp/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView
-from .models import Publishing
+from django.views.generic import ListView, DetailView
+from .models import Publishing, Book
 
 class HomeView(ListView):
     model  = Publishing
     template_name = 'home.html'
 
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+    context_object_name = 'book'
