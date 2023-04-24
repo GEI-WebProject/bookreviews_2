09dacc72b58ac7a70d6bdf8f9a2b21eec8254b5a
Mihaela <mbuturuga@gmail.com>
2023-04-24 23:20:28 +0200

    changed model for BookDetailView

--- bookapp/views.py
+++ bookapp/views.py
@@ -5,7 +5,8 @@ class HomeView(ListView):
     model  = Publishing
     template_name = 'home.html'
 
+
 class BookDetailView(DetailView):
-    model = Book
+    model = Publishing
     template_name = 'book_detail.html'
-    context_object_name = 'book'
+    context_object_name = 'publishing'
