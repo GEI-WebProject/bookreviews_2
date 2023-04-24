d0db8ea7f9879a83b163e0618932a2dbb5a0fbd6
Mihaela <mbuturuga@gmail.com>
2023-04-24 12:36:50 +0200

    uptaded home as ListView

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Book
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model  = Book
     template_name = 'home.html'
 
