66af76eb9d863de03af547c8fe42439b8225cd4b
jrariza <joseramonarizaperez@gmail.com>
2023-05-06 21:18:22 +0200

    main page modified

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,14 +1,23 @@
 from django.views.generic import ListView, DetailView
 from .models import Book, Author
+from django.core.cache import cache
+from django.utils import timezone
+from random import sample
 
 class HomeView(ListView):
     model  = Book
     template_name = 'home.html'
     context_object_name = 'books'
-    paginate_by = 3
     
     def get_queryset(self):
-        return Book.objects.order_by('?')
+        today = timezone.now().date()
+        daily_books = cache.get(today)
+        if not daily_books:
+            # If the cached random books for today don't exist, generate them and cache them for the day
+            all_books = list(Book.objects.all())
+            daily_books = sample(all_books, 5)
+            cache.set(today, daily_books, 60 * 60 * 24)  # Cache for 24 hours
+        return daily_books
     
 
 class BookDetailView(DetailView):
