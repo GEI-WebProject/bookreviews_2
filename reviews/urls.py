70df5087c1ae81d0674838fe16218a846278172c
Mihaela <mbuturuga@gmail.com>
2023-05-09 14:03:42 +0200

    added reviews to nav bar

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', ReviewListView.as_view(), name="review_list"),
+    path('', ReviewListView.as_view(), name="reviews"),
     path('<int:pk>', ReviewDetailView.as_view(), name="review_detail"),
 ]
