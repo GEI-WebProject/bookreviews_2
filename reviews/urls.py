249d83d0d35b75cd636178ee2c6d53e8eb52b237
Mihaela <mbuturuga@gmail.com>
2023-05-09 11:53:11 +0200

    created review detail view

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', ReviewListView.as_view(), name="review_list"),
+    path('<int:pk>', ReviewDetailView.as_view(), name="review_detail"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -6,3 +6,8 @@ class ReviewListView(ListView):
     model = Review
     template_name = "reviews/reviews.html"
     context_object_name = "reviews"
+
+class ReviewDetailView(DetailView):
+    model = Review
+    template_name = "reviews/review_detail.html"
+    context_object_name = "review"    
