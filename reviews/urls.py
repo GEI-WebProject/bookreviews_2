15813684fe03719ded4790c815370ff2c0d561e9
Mihaela <mbuturuga@gmail.com>
2023-05-09 11:02:15 +0200

    created review list page

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = []
+urlpatterns = [
+    path('', ReviewListView.as_view(), name="review_list"),
+]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Review
 
-# Create your views here.
+class ReviewListView(ListView):
+    model = Review
+    template_name = "reviews/reviews.html"
+    context_object_name = "reviews"
