4d189892bb2837f8a897573980ab06b159f49b19
Mihaela <mbuturuga@gmail.com>
2023-05-11 00:03:58 +0200

    modified review model

--- reviews/models.py
+++ reviews/models.py
@@ -3,14 +3,24 @@ from django.contrib.auth.models import User
 from bookapp.models import Book
 
 # Create your models here.
+
+
 class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews")
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="reviews")
     title = models.CharField(max_length=50)
     body = models.TextField()
-    rating = models.IntegerField()
+    rating = models.PositiveIntegerField(choices=(
+        (1, 'Bad'),
+        (2, 'Poor'),
+        (3, 'Okay'),
+        (4, 'Good'),
+        (5, 'Excellent')
+    ))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f"{self.title} by {self.user}"
