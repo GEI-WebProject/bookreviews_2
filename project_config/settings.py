a8efcb0fd164a8237a4e0337587a56fcf8786ce0
jrariza <joseramonarizaperez@gmail.com>
2023-05-11 01:13:04 +0200

    added success messages

--- project_config/settings.py
+++ project_config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+from django.contrib.messages import constants as messages
+
 
 env = environ.Env()
 
@@ -138,3 +140,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+}
