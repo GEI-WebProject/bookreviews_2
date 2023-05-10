b9b27c8efe0f9074ab4733d15d4b38f99b0dba7d
Mihaela <mbuturuga@gmail.com>
2023-05-10 23:33:45 +0200

    fixed reviews urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('reviews/', AllReviewsListView.as_view(), name="reviews"),
     path('reviews/<int:pk>', ReviewDetailView.as_view(), name="review_detail"),
     path('books/<int:book_id>/reviews', BookReviewsListView.as_view(), name="book_reviews"),
-    path('books/<int:book_id>/reviews/new', BookReviewsCreateView.as_view(), name="review_create"),
-    path('books/<int:book_id>/reviews/<int:pk>/edit', BookReviewsUpdateView.as_view(), name="review_update"),
-    path('books/<int:book_id>/reviews/<int:pk>/delete', BookReviewsDeleteView.as_view(), name="review_delete"),
+    path('books/<int:book_id>/reviews/new', CreateReviewView.as_view(), name="review_create"),
+    path('books/<int:book_id>/reviews/<int:pk>/edit', UpdateReviewView.as_view(), name="review_update"),
+    path('books/<int:book_id>/reviews/<int:pk>/delete', DeleteReviewView.as_view(), name="review_delete"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -6,15 +6,18 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import ReviewForm
 from bookapp.models import Book
 
+
 class AllReviewsListView(ListView):
     model = Review
     template_name = "reviews/reviews.html"
     context_object_name = "reviews"
 
+
 class ReviewDetailView(DetailView):
     model = Review
     template_name = "reviews/review_detail.html"
     context_object_name = "review"
+
 
 class BookReviewsListView(ListView):
     model = Review
@@ -24,44 +27,47 @@ class BookReviewsListView(ListView):
 
     # Add get_queryset() method to filter reviews by book
     def get_queryset(self):
-        return Review.objects.filter(book=self.kwargs['book_id'])
-    
+        return Review.objects.filter(book=self.kwargs['book_id']).order_by('-created_at')
+
     # Add get_context_data() method to add book to context
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['book'] = Book.objects.get(pk=self.kwargs['book_id'])
         return context
-    
-class BookReviewsCreateView(LoginRequiredMixin, CreateView):
+
+
+class CreateReviewView(LoginRequiredMixin, CreateView):
     model = Review
     template_name = "reviews/review_create.html"
     form_class = ReviewForm
-    
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.instance.book_id = self.kwargs['book_id']
         return super().form_valid(form)
-    
+
     def get_success_url(self):
         return reverse_lazy('book_reviews', kwargs={'book_id': self.kwargs['book_id']})
-    
-class BookReviewsUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+
+
+class UpdateReviewView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Review
     template_name = "reviews/review_update.html"
     form_class = ReviewForm
-    
+
     def get_success_url(self):
         return reverse_lazy('book_reviews', kwargs={'book_id': self.kwargs['book_id']})
-    
+
     def test_func(self):
         return self.request.user == self.get_object().user
-    
-class BookReviewsDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+
+
+class DeleteReviewView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Review
     template_name = "reviews/review_delete.html"
-    
+
     def get_success_url(self):
         return reverse_lazy('book_reviews', kwargs={'book_id': self.kwargs['book_id']})
-    
+
     def test_func(self):
         return self.request.user == self.get_object().user
