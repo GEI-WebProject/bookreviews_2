5f54a5452a39720b06442c88e9eeaf8655a5877f
Mihaela <mbuturuga@gmail.com>
2023-05-10 13:02:10 +0200

    added book reviews view

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('reviews/', ReviewListView.as_view(), name="reviews"),
+    path('reviews/', AllReviewsListView.as_view(), name="reviews"),
     path('reviews/<int:pk>', ReviewDetailView.as_view(), name="review_detail"),
+    path('books/<int:book_id>/reviews', BookReviewsListView.as_view(), name="book_reviews"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Review
 
-class ReviewListView(ListView):
+class AllReviewsListView(ListView):
     model = Review
     template_name = "reviews/reviews.html"
     context_object_name = "reviews"
@@ -11,3 +11,8 @@ class ReviewDetailView(DetailView):
     model = Review
     template_name = "reviews/review_detail.html"
     context_object_name = "review"
+
+class BookReviewsListView(ListView):
+    model = Review
+    template_name = "reviews/book_reviews.html"
+    context_object_name = "reviews"
