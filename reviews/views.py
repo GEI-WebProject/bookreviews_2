157cde7929cbf70b59b68a12cf540df967c0fb96
Mihaela <mbuturuga@gmail.com>
2023-05-10 14:07:46 +0200

    updated book reviews template

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Review
+from bookapp.models import Book
 
 class AllReviewsListView(ListView):
     model = Review
@@ -17,6 +18,12 @@ class BookReviewsListView(ListView):
     template_name = "reviews/book_reviews.html"
     context_object_name = "reviews"
 
-    # TODO: Add get_queryset() method to filter reviews by book
+    # Add get_queryset() method to filter reviews by book
     def get_queryset(self):
         return Review.objects.filter(book=self.kwargs['book_id'])
+    
+    # Add get_context_data() method to add book to context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['book'] = Book.objects.get(pk=self.kwargs['book_id'])
+        return context
