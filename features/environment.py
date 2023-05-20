5d1f3daf453aa1421d985167c6ca6ddd13c2068b
jrariza <joseramonarizaperez@gmail.com>
2023-05-20 19:04:24 +0200

    modified environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,9 @@
 from splinter.browser import Browser
 
+
 def before_all(context):
-    context.browser = Browser('firefox', headless=True)
+    context.browser = Browser('firefox', headless=False)
+
 
 def after_all(context):
     context.browser.quit()
