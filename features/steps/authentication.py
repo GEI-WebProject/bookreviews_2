f911da5793aa65d563d601730273d2fd93adb7d7
ArnauRodri <arnaurodriguezgonzalez@gmail.com>
2023-05-20 21:41:38 +0200

    full signup test

--- features/steps/authentication.py
+++ features/steps/authentication.py
@@ -4,6 +4,30 @@ from behave import *
 use_step_matcher("parse")
 
 
+@given(u'I click on "Sign up" link')
+def step_impl(context):
+    context.browser.find_by_name('signup_link').first.click()
+
+
+@when(u'I fill the account form with valid data')
+def step_impl(context):
+    form = context.browser.find_by_name('signup_form')
+    for row in context.table:
+        for heading in row.headings:
+            context.browser.fill(heading, row[heading])
+
+
+@when(u'I click on the "Sign up" button')
+def step_impl(context):
+    context.browser.find_by_name('signup_button').first.click()
+
+
+@then(u'I am logged in as "{username}"')
+def step_impl(context, username):
+    user_section = context.browser.find_by_name('user_session').first.value
+    assert user_section.find(username) != -1
+
+
 @when(u'I click on "Log in" link')
 def step_impl(context):
     context.browser.find_by_name("login_link").first.click()
--- features/steps/location.py
+++ features/steps/location.py
@@ -4,6 +4,11 @@ from behave import *
 use_step_matcher("parse")
 
 
+@then(u'I am redirected to the main page')
+def step_impl(context):
+    assert context.browser.url == context.get_url('home')
+
+
 @given(u'I am on the main page')
 def step_impl(context):
     context.browser.visit(context.get_url('home'))
